217e122c5d3b627539dd8f149548c892dbe16669
Tatyana <Tatyana.kaleda.work@gmail.com>
2020-11-13 03:01:44 +0300

    mobile version (without 5section)

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
         dots: true,
         infinite: true,
         slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ],
         prevArrow: $('.prev'),
         nextArrow: $('.next')
     });
